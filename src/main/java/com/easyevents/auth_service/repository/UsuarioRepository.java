28567673d1ee879a2c14c3c1a7d706b2ac5136b7
FelipeMDB-UNESP <105891789+FelipeMDB-UNESP@users.noreply.github.com>
2025-05-29 19:23:16 -0300

    crud

--- src/main/java/com/easyevents/auth_service/repository/UsuarioRepository.java
+++ src/main/java/com/easyevents/auth_service/repository/UsuarioRepository.java
@@ -3,5 +3,9 @@ package com.easyevents.auth_service.repository;
 import com.easyevents.auth_service.domain.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends MongoRepository<Usuario, String> {
+
+    Optional<Usuario> findByEmail(String email);
 }
