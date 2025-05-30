1e425da8e5888097c01e07a4b5f271d1dd2bddfe
FelipeMDB-UNESP <felipe.britto@unesp.br>
2025-05-30 01:07:21 -0300

    Mais alterações de nomenclatura

--- src/main/java/com/easyevents/auth_service/domain/model/Convidado.java
+++ src/main/java/com/easyevents/auth_service/domain/model/Convidado.java
@@ -2,20 +2,17 @@ package com.easyevents.auth_service.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-import org.springframework.data.annotation.Id;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-import java.util.Map;
 
 @Document(collection = "convidado")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Setter
 @Getter
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Convidado extends Pessoa {
+public class Convidado extends PessoaModel {
 
     private StatusConfirmacao conviteStatus;
 
